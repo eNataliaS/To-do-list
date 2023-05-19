43d52c5c63f0919c367fce24a456e138945d1edc
Natalia Szozda <natalia.szozda0@gmail.com>
2023-05-19 16:53:30 +0200

    added new task and after focus

--- js.js
+++ js.js
@@ -58,13 +58,15 @@
   const onFormSubmit = (event) => {
     event.preventDefault();
 
-    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    const newTaskElement = document.querySelector(".js-input");
+        const newTaskContent = newTaskElement.value.trim();
 
-    if (newTaskContent === "") {
-      return;
-    }
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            newTaskElement.value = "";
+        }
 
-    addNewTask(newTaskContent);
+        newTaskElement.focus();
   };
 
   const init = () => {
